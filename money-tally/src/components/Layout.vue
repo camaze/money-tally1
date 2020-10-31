b69cf0a1cb77a49ec87283dcb26faa52d53f068e
Nian <1046783533@qq.com>
2020-10-31 10:54:05 +0800

    创建Layout组件

---
+++ money-tally/src/components/Layout.vue
@@ -0,0 +1,32 @@
+<!--几种状态都会用到的布局减少重复-->
+<template>
+  <div class="nav-wrapper">
+    <div class="content">
+      <slot/>
+    </div>
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Nav from './Nav.vue'
+  export default {
+    name: 'Layout',
+    components:{Nav}
+  };
+</script>
+
+<style lang="scss" scoped>
+  .nav-wrapper {
+    display: flex;
+    border: 1px solid green;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .content {
+    flex-grow: 1;
+    overflow: auto;
+    border: 1px solid blue;
+  }
+</style>
--- money-tally/src/views/Money.vue
+++ money-tally/src/views/Money.vue
@@ -1,31 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-      <p>Money.vue</p>
-
-    </div>
-
-    <Nav/>
-  </div>
+  <Layout>
+    <p>Money.vue</p>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -34,18 +10,3 @@
     name: 'Money',
   };
 </script>
-
-<style lang="scss" scoped>
-  .nav-wrapper {
-    display: flex;
-    border: 1px solid green;
-    flex-direction: column;
-    height: 100vh;
-  }
-
-  .content {
-    flex-grow: 1;
-    overflow: auto;
-    border: 1px solid blue;
-  }
-</style>
