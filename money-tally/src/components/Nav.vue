490c7dad9d984578a6a21a6d977c67e05cd3b8ac
Nian <1046783533@qq.com>
2020-10-31 21:30:07 +0800

    创建Icon组件

--- money-tally/src/components/Nav.vue
+++ money-tally/src/components/Nav.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
-    |
-    <svg>
-      <use xlink:href="#money"></use>
-    </svg>
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <!--    <svg>-->
+    <!--      <use xlink:href="#money"></use>-->
+    <!--    </svg>-->
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
+
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
 
   </div>
 </template>
 
 <script lang="ts">
-  import x from '@/assets/icons/money.svg'
-  console.log(x)
+
   export default {
     name: 'Nav'
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
+  .nav {
     border: 1px solid red;
   }
 </style>
