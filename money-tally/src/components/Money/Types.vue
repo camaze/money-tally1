ff7b04cfc99b546ccb9bc4b23ebafe5a3a0f88ca
Nian <1046783533@qq.com>
2020-11-01 17:53:56 +0800

    使用JS实现了Types组件的功能

--- money-tally/src/components/Money/Types.vue
+++ money-tally/src/components/Money/Types.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected' "
+          @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected' "
+          @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script>
   export default {
-    name: 'Types'
+    name: 'Types',
+    data(){
+      return{
+        type: '-'    // 默认为-， -为支出，+为收入
+      }
+    },
+    methods:{
+      selectType(type){
+        if(type !== '-' && type !== '+'){
+          throw new Error('type is wrong!')
+        }
+        this.type = type
+      }
+    }
   };
 </script>
 
@@ -27,7 +42,7 @@
       justify-content: center;
       align-items: center;
       position: relative;
-
+      // 伪类和绝对定位
       &.selected::after {
         content: '';
         position: absolute;
