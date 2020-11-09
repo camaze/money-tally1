e087bd4274550468d1282905722c188353b36c58
Nian <1046783533@qq.com>
2020-11-09 10:46:04 +0800

    抽离 Tabs.vue 使用在 Statistics.vue 里面

---
+++ money-tally/src/components/Tabs.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul class="tabs">
+    <li v-for="item in dataSource" :key="item.value"
+        :class="liClass(item)"
+        @click="select(item)">{{item.text}}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  type DataSourceItem = { text: string; value: string }
+
+  @Component
+  export default class Tabs extends Vue {
+    @Prop({required: true, type: Array})
+    dataSource!: DataSourceItem[];
+    @Prop(String)
+    readonly value!: string;    // value: 选中支出和收入
+    @Prop(String)
+    classPrefix?: string;       // 类的前缀，选择器好选
+
+    liClass(item: DataSourceItem) {
+      return {
+        [this.classPrefix + '-tabs-item']: this.classPrefix,
+        selected: item.value === this.value
+      };
+    }
+
+
+    select(item: DataSourceItem) {
+      this.$emit('update:value', item.value);
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tabs {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      // 伪类和绝对定位
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        height: 4px;
+        width: 100%;
+        background: #333;
+      }
+    }
+  }
+</style>
